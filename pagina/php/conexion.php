57810be3f66ddc3eaebad59a6f911c2a2ca0210f
Matiasjs1 <162461241+Matiasjs1@users.noreply.github.com>
2024-09-23 14:07:49 -0300

    Update conexion.php

--- pagina/php/conexion.php
+++ pagina/php/conexion.php
@@ -1,16 +1,16 @@
 <?php
 
-    /*$host = "bafibxyhsgmttezy2etd-mysql.services.clever-cloud.com";          
+    $host = "bafibxyhsgmttezy2etd-mysql.services.clever-cloud.com";          
     $usuario = "uayese2deroksko6";
     $contrasena = "RJaOeDA6GoAuidAI2Jul";
     $base_de_datos = "bafibxyhsgmttezy2etd";
-   */
- 
+   
+    /*
     $host = "localhost";          
     $usuario = "root";
     $contrasena = "";
     $base_de_datos = "reservas_paintball";
- 
+    */
 
     $conexion = mysqli_connect($host, $usuario, $contrasena, $base_de_datos); 
     /*
